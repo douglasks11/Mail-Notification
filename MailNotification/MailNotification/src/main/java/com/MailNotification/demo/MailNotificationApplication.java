99f1cbc3a3670ba1c5bbd446732b62e2f72528d7
Douglas Oliveira <douglas.oliveira4@outlook.com>
2020-08-12 12:24:36 -0300

    primeira_versao

---
+++ MailNotification/MailNotification/src/main/java/com/MailNotification/demo/MailNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.MailNotification.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailNotificationApplication.class, args);
+	}
+
+}
